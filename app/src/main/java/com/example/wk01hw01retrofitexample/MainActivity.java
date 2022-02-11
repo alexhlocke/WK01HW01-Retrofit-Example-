0c0540c6d9b2f5df35525817e7c0b9e01588ca1a
Alex Locke <alexhlocke@gmail.com>
2022-02-10 18:34:14 -0800

    Created project

---
+++ app/src/main/java/com/example/wk01hw01retrofitexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wk01hw01retrofitexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
